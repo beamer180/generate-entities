2e56943b8804638df631e663580d62a585d52fcf
beamer180 <eric.d.larsen@gmail.com>
2018-03-18 17:17:29 -0600

    refactored entity file parser, added entity builder

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 var GenerateEntities = require("../lib/generate-entities");
 var EntitiesFileParser = require('../lib/entities-file.parser');
+var GeneratorBuilder = require('../lib/generator.builder.js');
+const appRoot = require('app-root-dir').get();
 
-generateEntities = new GenerateEntities(process.argv, EntitiesFileParser);
+generateEntities = new GenerateEntities(process.argv, EntitiesFileParser, new GeneratorBuilder(appRoot), appRoot);
 generateEntities.run();
