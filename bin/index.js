636ab79c50deb2ae114bab60c007c841166f0303
beamer180 <eric.d.larsen@gmail.com>
2018-03-18 22:23:38 -0600

    WIP: refactored, started file writer, created file writer factory, getting hooks set up

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 var GenerateEntities = require("../lib/generate-entities");
 var EntitiesFileParser = require('../lib/entities-file.parser');
-var GeneratorBuilder = require('../lib/generator.builder.js');
+var GeneratorFactory = require('../lib/generator.builder.js');
+var FileWriterFactory = require('../lib/file-writer.factory');
+var FileWriter = require('../lib/file.writer');
 const appRoot = require('app-root-dir').get();
 
-generateEntities = new GenerateEntities(process.argv, EntitiesFileParser, new GeneratorBuilder(appRoot), appRoot);
+var FileWriterHooks = require('../lib/file.writer.hooks');
+var fileWriterHooks = new FileWriterHooks();
+
+generateEntities = new GenerateEntities(process.argv, EntitiesFileParser, new GeneratorFactory(appRoot, fileWriterHooks), appRoot,  new FileWriterFactory(FileWriter, fileWriterHooks));
 generateEntities.run();
